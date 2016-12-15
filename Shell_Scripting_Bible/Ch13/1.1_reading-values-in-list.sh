f94c82cdeb80c23645656b20ed965f73f9956e31
Juan Antonio <jantonio@medium.com>
2016-12-14 23:19:28 -0800

    Another for cmd example reading from list && changing the last variable

--- Shell_Scripting_Bible/Ch13/1.1_reading-values-in-list.sh
+++ Shell_Scripting_Bible/Ch13/1.1_reading-values-in-list.sh
@@ -8,3 +8,9 @@ do
     echo "The next state in our growing list is ${state}"
 done
 echo $?
+sleep 2
+
+echo "Note that each time the for cmmd iterates through the list of values provided, it assigns the $state variable the NEXT VALUE in the list"
+echo "After the LAST ITERATION, the $state variable REMAINS VALID THROUGHOUT THE REMAINDER OF THE SHELL SCRIPT."
+echo "Meaning, it RETAINS THE LAST ITERATION VALUE (unless you intentionally change its value)"
+
