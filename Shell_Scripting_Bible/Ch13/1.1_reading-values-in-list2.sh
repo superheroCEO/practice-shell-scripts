8c509c4439137041d0594b8bb47375fc95ac8361
Juan Antonio <jantonio@medium.com>
2016-12-14 22:59:40 -0800

    3 for examples that read from a list

---
+++ Shell_Scripting_Bible/Ch13/1.1_reading-values-in-list2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for name in Taylor TS "Taylor Swift" "Taylor Alison Swift" Tswizzle "T-swift" "T-swizzle"
+do
+    echo "You are my dear $name, are absolutely gorgeous and brilliant. I love your mind."
+done
+echo $?
---
+++ Shell_Scripting_Bible/Ch13/1.1_reading-values-in-list3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Example 3 bc I keep messing up on little shit..."
+
+for album in "Taylor Swift" Fearless "Speak Now" Red "1989" T6
+do
+    echo "${album} is one of JA's favorite albums - of ALL-TIME!"
+done
+echo $?
