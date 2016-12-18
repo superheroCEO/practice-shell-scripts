13fb780566aa572d86ef93bd2119eb6cf1954424
Juan Antonio <jantonio@medium.com>
2016-12-18 14:30:11 -0800

    sample script & two cent intership thoughts

---
+++ medium-WIP/247-coach-daily-nutrition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+title="24/7 Coach check-in"
+text="Strategic Fitness Nutrition daily question"
+
+osascript -e "display notification \"$text\" with title \"$title\""
+
+#read - p "Please enter your
+
+echo "Q: Are, you, ready, to beast it sat MOFO!? Hike--> food --> UFC --> smoke/chill/food again. And open up your presents ;-)" | mail -s "SUPER IMPORTANT MESSAGE: OPEN NOW MOFO" thberry10@gmail.com
+
