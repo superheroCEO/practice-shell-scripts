c9e560fd2eaf40670102b1c5c6c6d2586a5927f0
Juan Antonio <jantonio@medium.com>
2016-12-18 16:27:39 -0800

    Add additional copy explanation to set up field separators

--- Shell_Scripting_Bible/Ch13/1.4_reading-values-from-command.sh
+++ Shell_Scripting_Bible/Ch13/1.4_reading-values-from-command.sh
@@ -14,3 +14,7 @@ echo $?
 
 echo "How did that work out?"
 
+echo "NOTE: This example uses the  cat  cmd in the COMMAND SUBSTITUTION to display the contents of the file states."
+echo "NOTICE the states files includes each state ON A SEPERATE LINE, NOT SEPERATED BY SPACES"
+echo "The  for  cmd still iterates through the output of the  cat  cmd ONE LINE AT A TIME, assuming that each state is on a seperate line."
+echo "HOWEVER, this doesn't solve the problem of having spaces in data. IF you list a state with a space in it, the  for  cmd still takes each word as a seperate value."
